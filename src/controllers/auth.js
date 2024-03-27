62a580c8e90304ace5dc0b4e09e982e13442d3e2
Camilo Perez <camilop15.cp@gmail.com>
2024-03-26 22:39:38 -0400

    feat: Add user register controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -6,10 +6,30 @@ const { ResponseMessage } = require('../utils/message')
  * @param {Response} res - Response object.
  */
 async function register(req, res) {
-  const message = new ResponseMessage({
-    message: 'This is the register route.'
-  })
-  res.send(message)
+  const { username, password, email } = req.body
+
+  const { error } = registerValidation({ username, password, email })
+  if (error) {
+    throw new HttpError({ httpStatusCode: 400, message: error.message })
+  }
+
+  const result = await findOneUser({ $or: [{ username }, { email }] })
+  if (result) {
+    throw new HttpError({
+      httpStatusCode: 400,
+      message: 'Username or Email aready exists.'
+    })
+  }
+
+  const hashedPassword = await hashPassword(password)
+  const user = await createOneUser(username, hashedPassword, email)
+  if (user) {
+    res.send(
+      new ResponseMessage({
+        message: `User ${user.username} has been created.`
+      })
+    )
+  }
 }
 
 /**
