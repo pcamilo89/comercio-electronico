00ab148234d22e5b70a23b86f8174b3bf600c6e9
Camilo Perez <camilop15.cp@gmail.com>
2024-03-28 13:59:57 -0400

    feat: Add update product validator

--- src/validators/product.js
+++ src/validators/product.js
@@ -19,6 +19,26 @@ function createProductValidation({ name, description, price, quantity }) {
   return schema.validate({ name, description, price, quantity })
 }
 
+/**
+ * Validate fields needed to update a product.
+ * @param {{ name?: string; description?: string; price?: number; quantity?: number; }} object - Object that contains needed fields.
+ * @param {string} [object.name] - Product name.
+ * @param {string} [object.description] - Product description.
+ * @param {number} [object.price] - Product price.
+ * @param {number} [object.quantity] - Product quantity avaiable in stock.
+ * @returns {object} Validation result.
+ */
+function updateProductValidation({ name, description, price, quantity }) {
+  const schema = Joi.object({
+    name: Joi.string().min(3),
+    description: Joi.string().min(3),
+    price: Joi.number().min(0),
+    quantity: Joi.number().min(0)
+  }).min(1)
+  return schema.validate({ name, description, price, quantity })
+}
+
 module.exports = {
-  createProductValidation
+  createProductValidation,
+  updateProductValidation
 }
