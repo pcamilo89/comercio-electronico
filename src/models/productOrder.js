237325607cadf9b05b89bb319ccb985ce7d9babd
Camilo Perez <camilop15.cp@gmail.com>
2024-03-29 18:47:33 -0400

    feat: Add product order model

---
+++ src/models/productOrder.js
@@ -0,0 +1,53 @@
+const { model, Schema } = require('mongoose')
+
+/**
+ * Product Schema
+ * @type {Schema}
+ */
+const productSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+    maxLength: 50
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+})
+
+/**
+ * ProductOrder Schema
+ * @type {Schema}
+ */
+const productOrderSchema = Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  products: {
+    type: [productSchema],
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['pending', 'approved', 'cancelled']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = {
+  ProductOrder: model('ProductOrder', productOrderSchema),
+  productOrderSchema
+}
