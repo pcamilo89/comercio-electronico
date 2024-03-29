36b463b24a585d2dd2b67d73a4895ef449ee2d4e
Camilo Perez <camilop15.cp@gmail.com>
2024-03-29 18:38:21 -0400

    feat: Add product order error

---
+++ src/errors/ProductOrderError.js
@@ -0,0 +1,22 @@
+const { HttpError } = require('./HttpError')
+
+/** Represents a custom HTTP error to be returned on any route. */
+class ProductOrderError extends HttpError {
+  /**
+   * Creates a Product related Error.
+   * @param {{ httpStatusCode?: number; message?: string; hasStack?: boolean; }} object - Information to send in the error message.
+   * @param {number} [object.httpStatusCode] - HTTP Status Code of the error.
+   * @param {string} [object.message] - Error message to send in response.
+   * @param {boolean} [object.hasStack] - Enable stack of the error.
+   */
+  constructor({
+    httpStatusCode = 400,
+    message = 'Product order related Error.',
+    hasStack = false
+  } = {}) {
+    super({ httpStatusCode, message, hasStack })
+    this.name = 'ProductOrderError'
+  }
+}
+
+module.exports = { ProductOrderError }
