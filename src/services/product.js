097489f6330f8d0a0a129a94e20b29d00fab80c5
Camilo Perez <camilop15.cp@gmail.com>
2024-03-29 21:02:08 -0400

    docs: Change find products description

--- src/services/product.js
+++ src/services/product.js
@@ -41,8 +41,8 @@ async function findOneProduct(filterBy, filterOut = undefined) {
  * @param {{ filterBy?: object; filterOut?: object; limit?: number; page?: number; }} object
  * @param {object} [object.filterBy] - Filter search by any parameter specified or a combination.
  * @param {object} [object.filterOut=undefined] - Filter out any parameter specified or a combination.
- * @param {number} [object.limit] - Limit amount of results to be returned, default is 10.
- * @param {number} [object.page] - Page to be returned, default is 1.
+ * @param {number} [object.limit] - Limit amount of results to be returned.
+ * @param {number} [object.page] - Page to be returned.
  * @returns {Promise<object>}
  */
 async function findProducts({ filterBy, filterOut = undefined, limit, page }) {
