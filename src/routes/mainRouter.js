da47e405b3055e8cbf72800dad1b4025502f0a43
Camilo Perez <camilop15.cp@gmail.com>
2024-03-31 00:49:05 -0400

    refac: Remove unnecessary functions in main router

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -16,26 +16,6 @@ mainRouter.use('/api/auth', authRouter)
 mainRouter.use('/api/products', productsRouter)
 mainRouter.use('/api/product-orders', productOrdersRouter)
 
-const { ResponseMessage } = require('../utils/message')
-const { HttpError } = require('../errors/HttpError')
-const { authHandler } = require('../middlewares/authHandler')
-
-mainRouter.get('/api', async (req, res) => {
-  const message = new ResponseMessage({ message: 'The server is working.' })
-  res.send(message)
-})
-
-mainRouter.get('/api/error', async (req, res) => {
-  throw new HttpError({ message: 'Test HTTP error is working.' })
-})
-
-mainRouter.get('/api/test', authHandler, async (req, res) => {
-  const message = new ResponseMessage({
-    message: 'This is a protected route, access successful.'
-  })
-  res.send(message)
-})
-
 mainRouter.use(notFoundHandler)
 mainRouter.use(errorHandler)
 
